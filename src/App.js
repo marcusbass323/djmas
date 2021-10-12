e0fbec2c9cddd29d3a8596bff0f30164f945a266
Marcus Bass <mbass@Marcuss-MBP.home>
2021-10-12 09:45:20 -0700

    commented out imports

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 //COMPONENTS
-import Mixes from './Components/Mixes/Mixes';
-import About from './Components/About/About';
-import Events from './Components/Events/Events';
-import Footer from './Components/Footer/Footer';
-import HeaderNav from './Components/HeaderNav/HeaderNav';
-import Carousel from './Components/Carousel/Carousel';
+// import Mixes from './Components/Mixes/Mixes';
+// import About from './Components/About/About';
+// import Events from './Components/Events/Events';
+// import Footer from './Components/Footer/Footer';
+// import HeaderNav from './Components/HeaderNav/HeaderNav';
+// import Carousel from './Components/Carousel/Carousel';
 
 function App() {
 
